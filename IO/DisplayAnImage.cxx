206157ac291f608df37489e9bbfed8513e7f3f69
Bill Lorensen <bill.lorensen@gmail.com>
2010-12-13 20:03:19 -0500

    SYNC: Files modified on wiki.

--- IO/DisplayAnImage.cxx
+++ IO/DisplayAnImage.cxx
@@ -10,7 +10,7 @@
 #include "vtkInteractorStyleImage.h"
 #include "vtkRenderer.h"
 #include "itkRGBPixel.h"
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
   if( argc < 2 )
     {
