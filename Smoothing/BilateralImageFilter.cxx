efad4ed8be4bda54b0680c4e5bc0fc6fc223e925
Bill Lorensen <bill.lorensen@gmail.com>
2010-12-19 20:03:47 -0500

    SYNC: Files modified on wiki.

--- Smoothing/BilateralImageFilter.cxx
+++ Smoothing/BilateralImageFilter.cxx
@@ -1,6 +1,7 @@
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 #include "itkBilateralImageFilter.h"
+#include "itkSubtractImageFilter.h"
  
 #include "QuickView.h"
  
@@ -28,26 +29,50 @@ int main(int argc, char * argv[])
   std::string inputFilename = argv[1];
  
   // Setup types
-  typedef itk::Image< unsigned char, 2 >   UnsignedCharImageType;
+  typedef itk::Image< float, 2 >   ImageType;
  
-  typedef itk::ImageFileReader< UnsignedCharImageType >  readerType;
- 
+  typedef itk::ImageFileReader< ImageType >
+    ReaderType;
   typedef itk::BilateralImageFilter<
-    UnsignedCharImageType, UnsignedCharImageType >  filterType;
+    ImageType, ImageType >
+    FilterType;
+  typedef itk::SubtractImageFilter< ImageType >
+    SubtractImageFilterType;
  
   // Create and setup a reader
-  readerType::Pointer reader = readerType::New();
+  ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( inputFilename.c_str() );
  
   // Create and setup a derivative filter
-  filterType::Pointer bilateralFilter = filterType::New();
+  FilterType::Pointer bilateralFilter = FilterType::New();
   bilateralFilter->SetInput( reader->GetOutput() );
   bilateralFilter->SetDomainSigma(domainSigma);
   bilateralFilter->SetRangeSigma(rangeSigma);
 
+  SubtractImageFilterType::Pointer diff = SubtractImageFilterType::New();
+  diff->SetInput1(reader->GetOutput());
+  diff->SetInput2(bilateralFilter->GetOutput());
+
   QuickView viewer;
-  viewer.AddImage(reader->GetOutput());
-  viewer.AddImage(bilateralFilter->GetOutput());
+  viewer.AddImage(
+    reader->GetOutput(),true,
+    itksys::SystemTools::GetFilenameName(argv[1]));  
+
+  std::stringstream desc;
+  desc << "Bilateral\ndomainSigma = " << domainSigma
+       << " rangeSigma = " << rangeSigma;
+  viewer.AddImage(
+    bilateralFilter->GetOutput(),
+    true,
+    desc.str());  
+
+  std::stringstream desc2;
+  desc2 << "Original - Bilateral";
+  viewer.AddImage(
+    diff->GetOutput(),
+    true,
+    desc2.str());  
+
   viewer.Visualize();
  
   return EXIT_SUCCESS;
--- Smoothing/MedianImageFilter.cxx
+++ Smoothing/MedianImageFilter.cxx
@@ -1,6 +1,8 @@
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 #include "itkMedianImageFilter.h"
+#include "itkSubtractImageFilter.h"
+
 #include "itksys/SystemTools.hxx"
 
 #include <sstream>
@@ -25,33 +27,45 @@ int main(int argc, char * argv[])
   std::string inputFilename = argv[1];
 
   // Setup types
-  typedef itk::Image< unsigned char, 2 >                 UnsignedCharImageType;
-  typedef itk::ImageFileReader< UnsignedCharImageType >  readerType;
-  typedef itk::MedianImageFilter<
-    UnsignedCharImageType, UnsignedCharImageType >       filterType;
+  typedef itk::Image<float, 2 >                         ImageType;
+  typedef itk::ImageFileReader<ImageType>               ReaderType;
+  typedef itk::MedianImageFilter<ImageType, ImageType > FilterType;
+  typedef itk::SubtractImageFilter<ImageType>           SubtractType;
 
   // Create and setup a reader
-  readerType::Pointer reader = readerType::New();
+  ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( inputFilename );
 
   // Create and setup a median filter
-  filterType::Pointer medianFilter = filterType::New();
-  filterType::SizeType kernelRadius;
+  FilterType::Pointer medianFilter = FilterType::New();
+  FilterType::SizeType kernelRadius;
   kernelRadius.Fill(radius);
   medianFilter->SetRadius(radius);
   medianFilter->SetInput( reader->GetOutput() );
 
+  SubtractType::Pointer diff = SubtractType::New();
+  diff->SetInput1(reader->GetOutput());
+  diff->SetInput2(medianFilter->GetOutput());
+
   QuickView viewer;
-  viewer.AddImage<UnsignedCharImageType>(
+  viewer.AddImage(
     reader->GetOutput(),true,
     itksys::SystemTools::GetFilenameName(inputFilename));  
 
   std::stringstream desc;
   desc << "MedianImageFilter, radius = " << radius;
-  viewer.AddImage<UnsignedCharImageType>(
+  viewer.AddImage(
     medianFilter->GetOutput(),
     true,
     desc.str());  
+
+  std::stringstream desc2;
+  desc2 << "Original - Median";
+  viewer.AddImage(
+    diff->GetOutput(),
+    true,
+    desc2.str());  
+
   viewer.Visualize();
 
   return EXIT_SUCCESS;
