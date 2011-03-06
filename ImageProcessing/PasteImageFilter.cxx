6c06eb758033a8ff829bad7a82b9e1f0320d1558
Bill Lorensen <bill.lorensen@gmail.com>
2011-03-05 20:03:10 -0500

    SYNC: Files modified on wiki.

--- ImageProcessing/PasteImageFilter.cxx
+++ ImageProcessing/PasteImageFilter.cxx
@@ -4,12 +4,7 @@
 
 #include "itkImageToVTKImageFilter.h"
 
-#include "vtkImageViewer.h"
-#include "vtkRenderWindowInteractor.h"
-#include "vtkSmartPointer.h"
-#include "vtkImageActor.h"
-#include "vtkInteractorStyleImage.h"
-#include "vtkRenderer.h"
+#include "QuickView.h"
 
 typedef itk::Image<unsigned char, 2>  ImageType;
 
@@ -27,97 +22,31 @@ int main(int, char *[])
   typedef itk::PasteImageFilter <ImageType, ImageType >
     PasteImageFilterType;
 
-  // The SetDestinationIndex() method prescribes where in the first input to start pasting data from the second input.
-  // The SetSourceRegion method prescribes the section of the second image to paste into the first.
+  // The SetDestinationIndex() method prescribes where in the first
+  // input to start pasting data from the second input.
+  // The SetSourceRegion method prescribes the section of the second
+  // image to paste into the first.
 
   ImageType::IndexType destinationIndex;
-  destinationIndex[0] = 110;
-  destinationIndex[1] = 110;
+  destinationIndex[0] = 10;
+  destinationIndex[1] = 10;
   
   PasteImageFilterType::Pointer pasteFilter
     = PasteImageFilterType::New ();
   pasteFilter->SetSourceImage(image1);
   pasteFilter->SetDestinationImage(image2);
-  pasteFilter->SetSourceRegion(image2->GetLargestPossibleRegion());
+  pasteFilter->SetSourceRegion(image1->GetLargestPossibleRegion());
   pasteFilter->SetDestinationIndex(destinationIndex);
-  pasteFilter->Update();
 
-  // Visualize first image
-  typedef itk::ImageToVTKImageFilter<ImageType> ConnectorType;
-  ConnectorType::Pointer connector1 = ConnectorType::New();
-  connector1->SetInput(image1);
+  QuickView viewer;
+  viewer.AddImage(
+    image1.GetPointer());
+  viewer.AddImage(
+    image2.GetPointer());
+  viewer.AddImage(
+    pasteFilter->GetOutput());
 
-  vtkSmartPointer<vtkImageActor> actor1 =
-    vtkSmartPointer<vtkImageActor>::New();
-  actor1->SetInput(connector1->GetOutput());
-
-  // Visualize second image
-  typedef itk::ImageToVTKImageFilter<ImageType> ConnectorType;
-  ConnectorType::Pointer connector2 = ConnectorType::New();
-  connector2->SetInput(image2);
-
-  vtkSmartPointer<vtkImageActor> actor2 =
-    vtkSmartPointer<vtkImageActor>::New();
-  actor2->SetInput(connector2->GetOutput());
-
-  // Visualize joined image
-  ConnectorType::Pointer pasteConnector = ConnectorType::New();
-  pasteConnector->SetInput(pasteFilter->GetOutput());
-
-  vtkSmartPointer<vtkImageActor> pasteActor =
-    vtkSmartPointer<vtkImageActor>::New();
-  pasteActor->SetInput(pasteConnector->GetOutput());
-
-  // There will be one render window
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
-  renderWindow->SetSize(900, 300);
-
-  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  interactor->SetRenderWindow(renderWindow);
-
-  // Define viewport ranges
-  // (xmin, ymin, xmax, ymax)
-  double leftViewport[4] = {0.0, 0.0, 0.33, 1.0};
-  double centerViewport[4] = {0.33, 0.0, 0.66, 1.0};
-  double rightViewport[4] = {0.66, 0.0, 1.0, 1.0};
-
-  // Setup both renderers
-  vtkSmartPointer<vtkRenderer> leftRenderer =
-    vtkSmartPointer<vtkRenderer>::New();
-  renderWindow->AddRenderer(leftRenderer);
-  leftRenderer->SetViewport(leftViewport);
-  leftRenderer->SetBackground(.6, .5, .4);
-
-  vtkSmartPointer<vtkRenderer> centerRenderer =
-    vtkSmartPointer<vtkRenderer>::New();
-  renderWindow->AddRenderer(centerRenderer);
-  centerRenderer->SetViewport(centerViewport);
-  centerRenderer->SetBackground(.4, .5, .6);
-  
-  vtkSmartPointer<vtkRenderer> rightRenderer =
-    vtkSmartPointer<vtkRenderer>::New();
-  renderWindow->AddRenderer(rightRenderer);
-  rightRenderer->SetViewport(rightViewport);
-  rightRenderer->SetBackground(.4, .5, .6);
-
-  // Add the sphere to the left and the cube to the right
-  leftRenderer->AddActor(actor1);
-  centerRenderer->AddActor(actor2);
-  rightRenderer->AddActor(pasteActor);
-
-  leftRenderer->ResetCamera();
-  centerRenderer->ResetCamera();
-  rightRenderer->ResetCamera();
-
-  renderWindow->Render();
-
-  vtkSmartPointer<vtkInteractorStyleImage> style =
-    vtkSmartPointer<vtkInteractorStyleImage>::New();
-  interactor->SetInteractorStyle(style);
-
-  interactor->Start();
+  viewer.Visualize();
 
   return EXIT_SUCCESS;
 }
@@ -166,8 +95,8 @@ void CreateImage2(ImageType::Pointer image)
   start[1] = 0;
 
   ImageType::SizeType size;
-  unsigned int NumRows = 20;
-  unsigned int NumCols = 20;
+  unsigned int NumRows = 400;
+  unsigned int NumCols = 500;
   size[0] = NumRows;
   size[1] = NumCols;
 
