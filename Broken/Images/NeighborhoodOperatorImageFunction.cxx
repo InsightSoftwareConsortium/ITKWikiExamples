a68c554759ccebcbeb6116f8a022125c7b8aaefb
Bill Lorensen <bill.lorensen@gmail.com>
2011-03-06 20:03:51 -0500

    SYNC: Files modified on wiki.

--- Broken/Images/NeighborhoodOperatorImageFunction.cxx
+++ Broken/Images/NeighborhoodOperatorImageFunction.cxx
@@ -2,103 +2,59 @@
 #include "itkNeighborhoodOperatorImageFunction.h"
 #include "itkNeighborhoodOperator.h"
 #include "itkNeighborhoodAllocator.h"
+#include "itkNeighborhoodOperatorImageFilter.h"
 
-#include <itkImageToVTKImageFilter.h>
+typedef itk::Image<unsigned char, 2>  UnsignedCharImageType;
+typedef itk::Image<float, 2>  FloatImageType;
 
-#include "vtkImageViewer.h"
-#include "vtkRenderWindowInteractor.h"
-#include "vtkSmartPointer.h"
-#include "vtkImageActor.h"
-#include "vtkInteractorStyleImage.h"
-#include "vtkRenderer.h"
-
-typedef itk::Image<unsigned char, 2>  ImageType;
-  
-void CreateImage(ImageType::Pointer image);
-
-class CustomOperator
-: public itk::NeighborhoodOperator<unsigned char, 2>
-{
-  // Count the number of valid neighbors and set the output pixel to that number.
-  // What we should see is that the number of neighbors is constant in the "middle"
-  // of the image and the number of neighbors is fewer on the edges.
-};
+void CreateImage(UnsignedCharImageType::Pointer image);
 
 int main(int, char*[])
 {
-
-  ImageType::Pointer image = ImageType::New();
+  UnsignedCharImageType::Pointer image = UnsignedCharImageType::New();
   CreateImage(image);
 
-  //CustomOperator customOperator;
-  //itkNeighborhoodOperatorImageFunction function;
-  //function->SetOperator(customOperator);
+  itk::Neighborhood<float, 2> neighborhood;
+  neighborhood.SetRadius(1);
+  
+  typedef itk::NeighborhoodOperatorImageFilter<UnsignedCharImageType, float> FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetOperator(neighborhood);
+  filter->SetInput(image);
+  filter->Update();
 
-  // Visualize
-  typedef itk::ImageToVTKImageFilter<ImageType> ConnectorType;
-  ConnectorType::Pointer connector = ConnectorType::New();
-  connector->SetInput(image);
-
-  vtkSmartPointer<vtkImageActor> actor =
-    vtkSmartPointer<vtkImageActor>::New();
-  actor->SetInput(connector->GetOutput());
-
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
-
-  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  interactor->SetRenderWindow(renderWindow);
-
-  vtkSmartPointer<vtkRenderer> renderer =
-    vtkSmartPointer<vtkRenderer>::New();
-  renderWindow->AddRenderer(renderer);
-
-  renderer->AddActor(actor);
-  renderer->ResetCamera();
-
-  renderWindow->Render();
-
-  vtkSmartPointer<vtkInteractorStyleImage> style =
-    vtkSmartPointer<vtkInteractorStyleImage>::New();
-
-  interactor->SetInteractorStyle(style);
-
-  interactor->Start();
+  itk::Index<2> index;
+  index.Fill(10);
+  
+  float output = filter->EvaluateAtIndex(index);
 
   return EXIT_SUCCESS;
 }
 
-void CreateImage(ImageType::Pointer image)
+void CreateImage(UnsignedCharImageType::Pointer image)
 {
   // Create an image with 2 connected components
-  ImageType::RegionType region;
-  ImageType::IndexType start;
-  start[0] = 0;
-  start[1] = 0;
+  UnsignedCharImageType::IndexType start;
+  start.Fill(0);
 
-  ImageType::SizeType size;
-  unsigned int NumRows = 200;
-  unsigned int NumCols = 300;
-  size[0] = NumRows;
-  size[1] = NumCols;
+  UnsignedCharImageType::SizeType size;
+  size.Fill(100);
 
-  region.SetSize(size);
-  region.SetIndex(start);
+  UnsignedCharImageType::RegionType region(start,size);
 
   image->SetRegions(region);
   image->Allocate();
 
   // Make a square
   for(unsigned int r = 20; r < 80; r++)
-  {
-      for(unsigned int c = 20; c < 80; c++)
+    {
+    for(unsigned int c = 20; c < 80; c++)
       {
-          ImageType::IndexType pixelIndex;
-          pixelIndex[0] = r;
-          pixelIndex[1] = c;
+      UnsignedCharImageType::IndexType pixelIndex;
+      pixelIndex[0] = r;
+      pixelIndex[1] = c;
 
-          image->SetPixel(pixelIndex, 15);
+      image->SetPixel(pixelIndex, 15);
       }
-  }
+    }
 }
