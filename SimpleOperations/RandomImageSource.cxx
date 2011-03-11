45ed0846734f926d29eb45abbc777b790312c1c3
Bill Lorensen <bill.lorensen@gmail.com>
2011-03-10 20:03:40 -0500

    SYNC: Files modified on wiki.

--- SimpleOperations/RandomImageSource.cxx
+++ SimpleOperations/RandomImageSource.cxx
@@ -1,54 +1,24 @@
 #include "itkImage.h"
 #include "itkRandomImageSource.h"
 
-#include <itkImageToVTKImageFilter.h>
-
-#include "vtkImageViewer.h"
-#include "vtkRenderWindowInteractor.h"
-#include "vtkSmartPointer.h"
-#include "vtkImageActor.h"
-#include "vtkInteractorStyleImage.h"
-#include "vtkRenderer.h"
+#include "QuickView.h"
 
 int main(int, char *[])
 {
   typedef itk::Image< unsigned char, 2 >  ImageType;
 
+  itk::Size<2> size;
+  size.Fill(10);
+
   itk::RandomImageSource<ImageType>::Pointer randomImageSource =
     itk::RandomImageSource<ImageType>::New();
   randomImageSource->SetNumberOfThreads(1); // to produce non-random results
-  
-  typedef itk::ImageToVTKImageFilter<ImageType> ConnectorType;
-  ConnectorType::Pointer connector = ConnectorType::New();
-  connector->SetInput(randomImageSource->GetOutput());
+  randomImageSource->SetSize(size);
+  randomImageSource->Update();
 
-  vtkSmartPointer<vtkImageActor> actor =
-    vtkSmartPointer<vtkImageActor>::New();
-  actor->SetInput(connector->GetOutput());
-
-  // There will be one render window
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
-
-  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  interactor->SetRenderWindow(renderWindow);
-
-  vtkSmartPointer<vtkRenderer> renderer =
-    vtkSmartPointer<vtkRenderer>::New();
-  renderWindow->AddRenderer(renderer);
-
-  renderer->AddActor(actor);
-  renderer->ResetCamera();
-
-  renderWindow->Render();
-
-  vtkSmartPointer<vtkInteractorStyleImage> style =
-    vtkSmartPointer<vtkInteractorStyleImage>::New();
-
-  interactor->SetInteractorStyle(style);
-
-  interactor->Start();
+  QuickView viewer;
+  viewer.AddImage(randomImageSource->GetOutput());
+  viewer.Visualize();
 
   return EXIT_SUCCESS;
 }
