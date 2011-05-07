e6c970c946da495f0baf7a0f31c7a68933876f1d
Bill Lorensen <bill.lorensen@gmail.com>
2011-05-06 20:04:53 -0400

    SYNC: Files modified on wiki.

--- ImageProcessing/BinaryFunctorImageFilter.cxx
+++ ImageProcessing/BinaryFunctorImageFilter.cxx
@@ -1,41 +1,9 @@
-#include "itkVectorImage.h"
-#include "itkVector.h"
-#include "itkVariableLengthVector.h"
-#include "itkRigid2DTransform.h"
+#include "itkImage.h"
 #include "itkBinaryFunctorImageFilter.h"
+#include "itkAndImageFilter.h"
 
-typedef itk::Image<float, 2>  ImageType;
+typedef itk::Image<unsigned char, 2>  ImageType;
 static void CreateImage(ImageType::Pointer image);
-
-namespace Functor
-{
-template< class TPixel>
-class MySquaredDifference
-{
-public:
-  MySquaredDifference() {}
-  ~MySquaredDifference() {}
-  bool operator!=(const MySquaredDifference &) const
-  {
-    return false;
-  }
-
-  bool operator==(const MySquaredDifference & other) const
-  {
-    return !( *this != other );
-  }
-
-  inline TPixel operator()(const TPixel & A,
-                            const TPixel & B) const
-  {
-    const double dA = static_cast< double >( A );
-    const double dB = static_cast< double >( B );
-    const double diff = dA - dB;
-
-    return static_cast< TPixel >( diff * diff );
-  }
-};
-}
 
 int main(int, char *[])
 {
@@ -46,9 +14,9 @@ int main(int, char *[])
   ImageType::Pointer image2 = ImageType::New();
   CreateImage(image2);
   image2->FillBuffer(5);
-  
+
   typedef itk::BinaryFunctorImageFilter< ImageType, ImageType, ImageType,
-                            Functor::MySquaredDifference<ImageType::PixelType> > FilterType;
+                            itk::Functor::AND<ImageType::PixelType> > FilterType;
 
   FilterType::Pointer filter = FilterType::New();
   filter->SetInput1(image1);
@@ -57,7 +25,7 @@ int main(int, char *[])
 
   itk::Index<2> pixelIndex;
   pixelIndex.Fill(0);
-  
+
   ImageType::PixelType input1PixelValue = image1->GetPixel(pixelIndex);
   ImageType::PixelType input2PixelValue = image2->GetPixel(pixelIndex);
   ImageType::PixelType outputPixelValue = filter->GetOutput()->GetPixel(pixelIndex);
@@ -73,7 +41,7 @@ void CreateImage(ImageType::Pointer image)
 {
   ImageType::IndexType start;
   start.Fill(0);
-  
+
   ImageType::SizeType size;
   size.Fill(10);
 
