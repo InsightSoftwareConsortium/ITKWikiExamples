ee328c4a967f0b839913b9f0d5dd6dfdbf3f3360
Bill Lorensen <bill.lorensen@gmail.com>
2011-05-24 11:06:39 -0400

    SYNC: Files modified on wiki.

--- IO/ReadVectorImage.cxx
+++ IO/ReadVectorImage.cxx
@@ -3,6 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+  if (argc < 2)
+    {
+    std::cerr << "Usage: " << argv[0] << " imageFilename" << std::endl;
+    return EXIT_FAILURE;
+    }
   std::string filename = argv[1];
   
   typedef itk::VectorImage<float, 2>  ImageType;
