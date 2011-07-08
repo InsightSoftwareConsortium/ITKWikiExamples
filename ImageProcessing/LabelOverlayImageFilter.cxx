739b665542e124c45e8684ecd24dfd2cf6fd46c9
Bill Lorensen <bill.lorensen@gmail.com>
2011-07-07 20:07:40 -0400

    SYNC: Files modified on wiki.

--- ImageProcessing/LabelOverlayImageFilter.cxx
+++ ImageProcessing/LabelOverlayImageFilter.cxx
@@ -7,7 +7,7 @@
 #include "itkRGBPixel.h"
 
 typedef itk::Image<unsigned char, 2>  ImageType;
-void CreateImage(ImageType::Pointer image);
+static void CreateImage(ImageType::Pointer image);
 
 int main(int, char *[])
 {
