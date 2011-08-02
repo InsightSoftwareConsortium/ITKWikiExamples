e83004782a924f091e4c1ee0fb4fd1f89365b848
Bill Lorensen <bill.lorensen@gmail.com>
2011-08-01 20:05:35 -0400

    SYNC: Files modified on wiki.

--- Developer/itkOilPaintingImageFilter.h
+++ Developer/itkOilPaintingImageFilter.h
@@ -1,9 +1,9 @@
 #ifndef __itkOilPaintingImageFilter_h
 #define __itkOilPaintingImageFilter_h
-
+ 
 #include "itkImageToImageFilter.h"
 #include "itkNeighborhoodIterator.h"
-
+ 
 namespace itk
 {
 /** \class OilPaintingImageFilter
@@ -23,42 +23,42 @@ public:
   typedef ImageToImageFilter< TImage, TImage > Superclass;
   typedef SmartPointer< Self >        Pointer;
   typedef typename NeighborhoodIterator<TImage>::RadiusType RadiusType;
-
+ 
   /** Method for creation through the object factory. */
   itkNewMacro(Self);
-
+ 
   /** Run-time type information (and related methods). */
   itkTypeMacro(OilPaintingImageFilter, ImageToImageFilter);
-  
+ 
   itkSetMacro(NumberOfBins, unsigned int);
   itkGetConstMacro(NumberOfBins, const unsigned int);
-
+ 
   itkSetMacro(Radius, RadiusType);
   itkGetConstMacro(Radius, const RadiusType);
   void SetRadius(unsigned int radius);
-
+ 
 protected:
   OilPaintingImageFilter();
   ~OilPaintingImageFilter(){}
-
+ 
   virtual void BeforeThreadedGenerateData();
-
+ 
   /** Does the real work. */
-  virtual void ThreadedGenerateData(const OutputImageRegionType & outputRegionForThread, int threadId);
-
+  virtual void ThreadedGenerateData(const typename Superclass::OutputImageRegionType& outputRegionForThread, int threadId);
+ 
 private:
   OilPaintingImageFilter(const Self &); //purposely not implemented
   void operator=(const Self &);  //purposely not implemented
-
+ 
   typename TImage::ValueType m_Maximum, m_Minimum;
   typename NeighborhoodIterator<TImage>::RadiusType m_Radius;
   unsigned int m_NumberOfBins;
 };
 } //namespace ITK
-
-
+ 
+ 
 #ifndef ITK_MANUAL_INSTANTIATION
 #include "itkOilPaintingImageFilter.txx"
 #endif
-
+ 
 #endif // __itkOilPaintingImageFilter_h
