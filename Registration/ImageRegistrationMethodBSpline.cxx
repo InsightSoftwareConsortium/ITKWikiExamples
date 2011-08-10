98e552490349e7914105b0cc0f523ef1f99082c8
Bill Lorensen <bill.lorensen@gmail.com>
2011-08-09 20:06:29 -0400

    SYNC: Files modified on wiki.

--- Registration/ImageRegistrationMethodBSpline.cxx
+++ Registration/ImageRegistrationMethodBSpline.cxx
@@ -38,7 +38,7 @@ int main( int argc, char *argv[] )
                             SpaceDimension,
                             SplineOrder >     TransformType;
 #else
-  typedef itk::BSplineDeformableTransform<
+  typedef itk::BSplineTransform<
                             CoordinateRepType,
                             SpaceDimension,
                             SplineOrder >     TransformType;
