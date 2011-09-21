67f630c2726138d40b9792826ceb0532d393b2a6
Bill Lorensen <bill.lorensen@gmail.com>
2011-09-20 20:06:31 -0400

    SYNC: Files modified on wiki.

--- ImageProcessing/LaplacianSharpeningImageFilter.cxx
+++ ImageProcessing/LaplacianSharpeningImageFilter.cxx
@@ -1,46 +1,47 @@
 #include "itkImage.h"
 #include "itkImageFileReader.h"
-#include "itkImageFileWriter.h"
 #include "itkRescaleIntensityImageFilter.h"
 #include "itkLaplacianSharpeningImageFilter.h"
+
+#include "QuickView.h"
 
 int main(int argc, char * argv[])
 {
   // Verify command line arguments
-  if( argc < 3 )
+  if( argc < 2 )
     {
     std::cerr << "Usage: " << std::endl;
-    std::cerr << argv[0] << "inputImageFile output" << std::endl;
+    std::cerr << argv[0] << " inputImageFile" << std::endl;
     return EXIT_FAILURE;
     }
 
   // Parse command line arguments
   std::string inputFilename = argv[1];
-  std::string outputFilename = argv[2];
   
-  // Output argument
-  std::cout << "Input: " << inputFilename << std::endl;
-  std::cout << "Input: " << outputFilename << std::endl;
-  
-
   // Setup types
   typedef itk::Image< float,  2 >   FloatImageType;
 
   typedef itk::ImageFileReader< FloatImageType >  readerType;
   readerType::Pointer reader = readerType::New();
   reader->SetFileName(inputFilename);
-  reader->Update();
 
   typedef itk::LaplacianSharpeningImageFilter<FloatImageType, FloatImageType >  LaplacianSharpeningImageFilterType;
-  LaplacianSharpeningImageFilterType::Pointer laplacianSharpeningImageFilter = LaplacianSharpeningImageFilterType::New();
+  LaplacianSharpeningImageFilterType::Pointer laplacianSharpeningImageFilter =
+    LaplacianSharpeningImageFilterType::New();
   laplacianSharpeningImageFilter->SetInput( reader->GetOutput() );
-  laplacianSharpeningImageFilter->Update();
 
-  typedef itk::ImageFileWriter< FloatImageType >  ImageFileWriterType;
-  ImageFileWriterType::Pointer writer = ImageFileWriterType::New();
-  writer->SetFileName(outputFilename);
-  writer->SetInput(laplacianSharpeningImageFilter->GetOutput());
-  writer->Update();
-  
+  QuickView viewer;
+  viewer.AddImage(
+    reader->GetOutput(),true,
+    itksys::SystemTools::GetFilenameName(argv[1]));  
+
+  std::stringstream desc;
+  desc << "LaplacianSharpeningImageFilter";
+  viewer.AddImage(
+    laplacianSharpeningImageFilter->GetOutput(),
+    true,
+    desc.str());  
+
+  viewer.Visualize();
   return EXIT_SUCCESS;
 }
