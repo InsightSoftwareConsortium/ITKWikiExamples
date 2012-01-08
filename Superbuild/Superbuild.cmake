bb5789ed171706eebaee33657b25b81c0649973d
Bill Lorensen <lorensen@domU-12-31-39-0B-81-52.compute-1.internal>
2012-01-08 08:38:44 -0500

    ENH: Added Superbuild for VTK and ITK

---
+++ Superbuild/Superbuild.cmake
@@ -0,0 +1,43 @@
+find_package(Git)
+if(NOT GIT_FOUND)
+  message(ERROR "Cannot find git. git is required for Superbuild")
+endif()
+
+option( USE_GIT_PROTOCOL "If behind a firewall turn this off to use http instead." ON)
+
+set(git_protocol "git")
+
+include( ExternalProject )
+
+# Compute -G arg for configuring external projects with the same CMake gener    ator:
+if(CMAKE_EXTRA_GENERATOR)
+  set(gen "${CMAKE_EXTRA_GENERATOR} - ${CMAKE_GENERATOR}")
+else()
+  set(gen "${CMAKE_GENERATOR}" )
+endif()
+
+set( ITKWikiExamples_DEPENDENCIES )
+
+include( ${CMAKE_SOURCE_DIR}/External-VTK.cmake )
+list( APPEND ITKWikiExamples_DEPENDENCIES VTK )
+
+include( ${CMAKE_SOURCE_DIR}/External-ITK.cmake )
+list( APPEND ITKWikiExamples_DEPENDENCIES ITK )
+
+ExternalProject_Add( ITKWikiExamples
+  DEPENDS ${ITKWikiExamples_DEPENDENCIES}
+  DOWNLOAD_COMMAND ""
+  SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/..
+  BINARY_DIR ITKWikiExamples-build
+  CMAKE_GENERATOR ${gen}
+  CMAKE_ARGS
+    ${ep_common_args}
+    -DBUILD_SHARED_LIBS:BOOL=FALSE
+    -DBUILD_TESTING:BOOL=FALSE
+     # ITK
+    -DITK_DIR:PATH=${ITK_DIR}
+    # VTK
+    -DUSE_VTK:BOOL=ON
+    -DVTK_DIR:PATH=${VTK_DIR}
+  INSTALL_COMMAND ""
+)
