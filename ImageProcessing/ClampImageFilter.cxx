9d9965b9bc730bee3b5cbef04fd92b1c373b2661
Bill Lorensen <bill.lorensen@gmail.com>
2012-06-22 20:06:41 -0400

    SYNC: Files modified on wiki.

--- ImageProcessing/ClampImageFilter.cxx
+++ ImageProcessing/ClampImageFilter.cxx
@@ -2,25 +2,64 @@
 #include "itkImageFileReader.h"
 #include "itkClampImageFilter.h"
 
+typedef itk::Image<float, 2>  FloatImageType;
+typedef itk::Image<unsigned char, 2>  UnsignedCharImageType;
+
+static void CreateImage(FloatImageType* const image);
+
 int main(int argc, char *argv[])
 {
+  FloatImageType::Pointer image;
+
+  // No input image argument provided
   if(argc < 2)
     {
-    std::cerr << "Required: filename" << std::endl;
-    return EXIT_FAILURE;
+    image = FloatImageType::New();
+    CreateImage(image);
     }
-
-  typedef itk::Image<unsigned char, 2>  UnsignedCharImageType;
-  typedef itk::Image<float, 2>  FloatImageType;
-
-  typedef itk::ImageFileReader<FloatImageType> ReaderType;
-  ReaderType::Pointer reader = ReaderType::New();
-  reader->SetFileName(argv[1]);
+  else // Input image argument provided
+    {
+    typedef itk::ImageFileReader<FloatImageType> ReaderType;
+    ReaderType::Pointer reader = ReaderType::New();
+    reader->SetFileName(argv[1]);
+    reader->Update();
+    image = reader->GetOutput();
+    }
 
   typedef itk::ClampImageFilter< FloatImageType, UnsignedCharImageType > ClampFilterType;
   ClampFilterType::Pointer clampFilter = ClampFilterType::New();
-  clampFilter->SetInput(reader->GetOutput());
+  clampFilter->SetInput(image);
   clampFilter->Update();
 
   return EXIT_SUCCESS;
+}
+
+void CreateImage(FloatImageType* const image)
+{
+  // Create an image with 2 connected components
+  FloatImageType::IndexType corner = {{0,0}};
+
+  FloatImageType::SizeType size;
+  unsigned int NumRows = 200;
+  unsigned int NumCols = 300;
+  size[0] = NumRows;
+  size[1] = NumCols;
+
+  FloatImageType::RegionType region(corner, size);
+
+  image->SetRegions(region);
+  image->Allocate();
+
+  // Make a square
+  for(unsigned int r = 40; r < 100; r++)
+    {
+    for(unsigned int c = 40; c < 100; c++)
+      {
+      FloatImageType::IndexType pixelIndex;
+      pixelIndex[0] = r;
+      pixelIndex[1] = c;
+
+      image->SetPixel(pixelIndex, 15);
+      }
+    }
 }
