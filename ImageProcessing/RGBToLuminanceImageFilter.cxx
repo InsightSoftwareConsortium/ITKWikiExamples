e172457e4ee110ddb37340a7eb3b1b11e1f040d8
Bill Lorensen <bill.lorensen@gmail.com>
2012-07-01 20:07:37 -0400

    SYNC: Files modified on wiki.

--- ImageProcessing/RGBToLuminanceImageFilter.cxx
+++ ImageProcessing/RGBToLuminanceImageFilter.cxx
@@ -1,40 +1,78 @@
 #include "itkImage.h"
 #include "itkImageFileReader.h"
-#include "itkImageFileWriter.h"
-#include "itkNormalizeImageFilter.h"
 #include "itkRGBToLuminanceImageFilter.h"
 
-typedef itk::Image<itk::RGBPixel<unsigned char>, 2>  RGBImageType;
-typedef itk::Image<float, 2>  ScalarImageType;
+#include "QuickView.h"
+
+typedef itk::Image<itk::RGBPixel<unsigned char>, 2> RGBImageType;
+typedef itk::Image<unsigned char, 2>  ScalarImageType;
+
+static void CreateImage(RGBImageType* const image);
 
 int main(int argc, char *argv[])
 {
-  // Verify arguments
+  RGBImageType::Pointer image;
+
   if(argc < 2)
     {
-    std::cerr << "Required: input output" << std::endl;
-    return EXIT_FAILURE;
+    //std::cerr << "Required: filename" << std::endl;
+    //return EXIT_FAILURE;
+    image = RGBImageType::New();
+    CreateImage(image);
     }
+  else
+  {
+    typedef itk::ImageFileReader<RGBImageType> ReaderType;
+    ReaderType::Pointer reader = ReaderType::New();
+    reader->SetFileName(argv[1]);
+    image = reader->GetOutput();
+  }
 
-  // Parse arguments
-  std::string inputFileName = argv[1];
-  std::string outputFileName = argv[2];
-  
-  typedef itk::ImageFileReader<RGBImageType> ReaderType;
-  ReaderType::Pointer reader = ReaderType::New();
-  reader->SetFileName(inputFileName);
-  reader->Update();
-
-  typedef itk::RGBToLuminanceImageFilter< RGBImageType, ScalarImageType > LuminanceFilterType;
+  typedef itk::RGBToLuminanceImageFilter<RGBImageType, ScalarImageType> LuminanceFilterType;
   LuminanceFilterType::Pointer luminanceFilter = LuminanceFilterType::New();
-  luminanceFilter->SetInput(reader->GetOutput());
+  luminanceFilter->SetInput(image);
   luminanceFilter->Update();
 
-  typedef itk::ImageFileWriter<ScalarImageType> WriterType;
-  WriterType::Pointer writer = WriterType::New();
-  writer->SetFileName(outputFileName);
-  writer->SetInput(luminanceFilter->GetOutput());
-  writer->Update();
-  
+  QuickView viewer;
+  viewer.AddRGBImage(image.GetPointer());
+  //viewer.AddRGBImage(image.GetPointer());
+  viewer.AddImage(luminanceFilter->GetOutput());
+  viewer.Visualize();
+
   return EXIT_SUCCESS;
+}
+
+
+void CreateImage(RGBImageType* const image)
+{
+  // Create an image with 2 connected components
+  itk::Index<2> corner = {{0,0}};
+
+  itk::Size<2> size;
+  unsigned int NumRows = 200;
+  unsigned int NumCols = 300;
+  size[0] = NumRows;
+  size[1] = NumCols;
+
+  itk::ImageRegion<2> region(corner, size);
+
+  image->SetRegions(region);
+  image->Allocate();
+
+  RGBImageType::PixelType pixel;
+  pixel.SetRed(200);
+  pixel.SetGreen(50);
+  pixel.SetBlue(50);
+  // Make a square
+  for(unsigned int r = 40; r < 100; r++)
+    {
+    for(unsigned int c = 40; c < 100; c++)
+      {
+      itk::Index<2> pixelIndex;
+      pixelIndex[0] = r;
+      pixelIndex[1] = c;
+
+      image->SetPixel(pixelIndex, pixel);
+      }
+    }
 }
