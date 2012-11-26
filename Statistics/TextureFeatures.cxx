02439c0aba0bb6b4ceba305dac8a0599b79cfc5b
Bill Lorensen <bill.lorensen@gmail.com>
2012-11-26 12:05:09 -0500

    ENH: ScrapeWiki now creates tarballs

--- Statistics/TextureFeatures.cxx
+++ Statistics/TextureFeatures.cxx
@@ -89,7 +89,7 @@ int main(int argc, char*argv[])
 {
   if(argc < 2)
     {
-    std::cerr << "Required arguments: image.mha" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " Required image.mha" << std::endl;
     return EXIT_FAILURE;
     }
   
