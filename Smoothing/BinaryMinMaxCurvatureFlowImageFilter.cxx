dd48deaf719db71bbe6432b8b87587eb4ab7d2e0
Bill Lorensen <bill.lorensen@gmail.com>
2012-11-30 17:15:59 -0500

    SYNC: Files modified on wiki.

--- Smoothing/BinaryMinMaxCurvatureFlowImageFilter.cxx
+++ Smoothing/BinaryMinMaxCurvatureFlowImageFilter.cxx
@@ -1,34 +1,38 @@
 #include "itkImage.h"
 #include "itkImageFileReader.h"
-#include "itkImageFileWriter.h"
 #include "itkBinaryMinMaxCurvatureFlowImageFilter.h"
+#include "itkSubtractImageFilter.h"
+
+#include "itksys/SystemTools.hxx"
+
+#include "QuickView.h"
 
 int main(int argc, char* argv[])
 {
-  if( argc != 4 )
+  if( argc < 2 )
     {
-    std::cerr << argv[0] << " InputFileName OutputFileName NumberOfIterations" <<std ::endl;
+    std::cerr << argv[0] << " InputFileName [NumberOfIterations]" <<std ::endl;
     return EXIT_FAILURE;
     }
 
   std::string inputFileName = argv[1];
-  std::string outputFileName = argv[2];
-  std::stringstream ssNumIter;
-  ssNumIter << argv[3];
-  unsigned int numberOfIterations;
-  ssNumIter >> numberOfIterations;
 
-  const unsigned int Dimension = 3;
+  unsigned int numberOfIterations = 2;
+  if (argc > 2)
+    {
+    numberOfIterations = atoi(argv[2]);
+    }
 
-  typedef unsigned char InputPixelType;
-  typedef float         OutputPixelType;
+  const unsigned int Dimension = 2;
+
+  typedef float InputPixelType;
+  typedef float OutputPixelType;
 
   typedef itk::Image< InputPixelType, Dimension >   InputImageType;
   typedef itk::ImageFileReader< InputImageType >    ReaderType;
 
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName(inputFileName);
-  reader->Update();
 
   typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
 
@@ -37,13 +41,33 @@ int main(int argc, char* argv[])
   filter->SetInput( reader->GetOutput() );
   filter->SetThreshold( 255 );
   filter->SetNumberOfIterations(numberOfIterations);
-  filter->Update();
 
-  typedef itk::ImageFileWriter< OutputImageType > WriterType;
-  WriterType::Pointer writer = WriterType::New();
-  writer->SetInput( filter->GetOutput() );
-  writer->SetFileName(outputFileName);
-  writer->Update();
+  typedef itk::SubtractImageFilter<OutputImageType>           SubtractType;
+  SubtractType::Pointer diff = SubtractType::New();
+  diff->SetInput1(reader->GetOutput());
+  diff->SetInput2(filter->GetOutput());
+
+  QuickView viewer;
+  viewer.AddImage(
+    reader->GetOutput(),true,
+    itksys::SystemTools::GetFilenameName(inputFileName));  
+
+  std::stringstream desc;
+  desc << "BinaryMinMaxCurvature, iterations = "
+       << numberOfIterations;
+  viewer.AddImage(
+    filter->GetOutput(),
+    true,
+    desc.str());  
+
+  std::stringstream desc2;
+  desc2 << "Original - BinaryMinMaxCurvatureFlow";
+  viewer.AddImage(
+    diff->GetOutput(),
+    true,
+    desc2.str());  
+
+  viewer.Visualize();
 
   return EXIT_SUCCESS;
 }
