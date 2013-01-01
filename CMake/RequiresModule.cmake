40859bfa822ea2adf2061e09b8a67c07d810ef4e
Bill Lorensen <bill.lorensen@gmail.com>
2013-01-01 14:54:34 -0500

    ENH: Initial support as an ITK remote module.
    Add itk-module.cmake to create the ITKWikiExamplesModule
    Add WikiExamples.remote.cmake to enable ITKWikiExaples as a remote
    module for ITKv4
    Add support for an ITK-supplied ItkVtkGlue if present

---
+++ CMake/RequiresModule.cmake
@@ -0,0 +1,8 @@
+macro(Requires_Module example module)
+  if(NOT ITK${module}_LOADED)
+    message(STATUS "ITKWikiExamples: ${example} requires ITK${module} but ITK${module} is not loaded")
+    string(REGEX REPLACE "[^;]*${example}.cxx"
+           "" ALL_FILES "${ALL_FILES}")
+  endif()
+endmacro()
+
---
+++ WikiExamples.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(WikiExamples
+  "A collection of examples that illsutare how to use ITK."
+  GIT_REPOSITORY https://git.gitorious.org/itkwikiexamples/itkwikiexamples.git
+  GIT_TAG master
+  )
