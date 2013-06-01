1bcb859c6f2e6443ae36d3c5a19ce3c97fb8f6d2
Bill Lorensen <bill.lorensen@gmail.com>
2013-06-01 13:54:13 -0400

    COMP: Update vtk version for superbuild

--- Superbuild/External-VTK.cmake
+++ Superbuild/External-VTK.cmake
@@ -4,7 +4,7 @@
 #-----------------------------------------------------------------------------
 # April 19, 2011
 set( VTK_TAG "be4c4f115793598f6c8937a98bdd1f227d471540" )
-set( VTK_TAG release )
+set( VTK_TAG v5.10.1 )
 
 ExternalProject_Add(VTK
   GIT_REPOSITORY "${git_protocol}://vtk.org/VTK.git"
