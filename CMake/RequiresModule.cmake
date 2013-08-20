54735a6656f9afcc48a64d4cbbebd127a86f4280
Bill Lorensen <bill.lorensen@gmail.com>
2013-08-20 14:56:44 -0400

    BUG: Improper syntax for module detection.

--- CMake/RequiresModule.cmake
+++ CMake/RequiresModule.cmake
@@ -1,5 +1,5 @@
 macro(Requires_Module example module)
-  if(NOT ITK${module}_LOADED AND NOT Module_ITK$(module))
+  if(NOT ITK${module}_LOADED AND NOT Module_ITK${module})
     message(STATUS "ITKWikiExamples: ${example} requires ITK${module} but ITK${module} is not loaded")
     string(REGEX REPLACE "[^;]*${example}.cxx"
            "" ALL_FILES "${ALL_FILES}")
