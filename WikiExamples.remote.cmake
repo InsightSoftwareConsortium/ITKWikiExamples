6e5abbd12048087d8df579cb31fe60932dc3845b
Bill Lorensen <lorensen@localhost.localdomain>
2014-06-15 13:27:39 -0400

    ENH: Some remote module cleanup

--- WikiExamples.remote.cmake
+++ WikiExamples.remote.cmake
@@ -1,26 +1,11 @@
 #
 # ITK WikiExamples
-#  This remote module require a VTK build
-#  The following CMake variable must be set for the ITK build
-#  BUILD_EXAMPLES:BOOL=OFF
-#  ITKV3_COMPATIBILITY:BOOL=ON
-#  Module_ITKVtkGlue:BOOL=ON
-# 
-# Check for requires cmake variables
-if (BUILD_EXAMPLES)
-  message(FATAL_ERROR "WikiExamples: The ITK build must be configured with BUILD_EXAMPLES:BOOL=OFF")
-endif()
-
-if (NOT ITKV3_COMPATIBILITY)
-  message(FATAL_ERROR "WikiExamples: The ITK build must be configured with ITKV3_COMPATIBILITY:BOOL=ON")
-endif()
-
-if (NOT Module_ITKVtkGlue)
-  message(FATAL_ERROR "WikiExamples: The ITK build must be configured with Module_ITKVtkGlue:BOOL=ON")
-endif()
+#  This remote module requires a VTK build
+#
 
 itk_fetch_module(WikiExamples
   "A collection of examples that illustrate how to use ITK."
   GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/ITKWikiExamples.git
-  GIT_TAG master
+# June 13, 2014
+  GIT_TAG f1499b4ded619ecb6932e05f0ff6e298b58aa6b8
   )
