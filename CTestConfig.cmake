faff65c7476c856f9ecacc53f4d31186eff4e1bf
Bill Lorensen <bill.lorensen@gmail.com>
2014-06-23 08:07:10 -0400

    BUG: Missing slash on CTEST_DROP_LOCATION

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -9,5 +9,5 @@ set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "open.cdash.org")
-set(CTEST_DROP_LOCATION "submit.php?project=ITKWikiExamples")
+set(CTEST_DROP_LOCATION "/submit.php?project=ITKWikiExamples")
 set(CTEST_DROP_SITE_CDASH TRUE)
