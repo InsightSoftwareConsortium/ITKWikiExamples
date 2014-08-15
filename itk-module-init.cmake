1ac202f0d5a321620e32b8082051ebd332fee138
Bill Lorensen <lorensen@localhost.localdomain>
2014-08-15 13:07:40 -0400

    ENH: Initialize Wiki Examples with required VTK modules
    If being built by as a remote module, specify the required VTK
    modules.

---
+++ itk-module-init.cmake
@@ -0,0 +1,21 @@
+#
+# Find the packages required by this module
+#
+
+# Look for VTK
+message(STATUS "ITKWikiExamples: Finding VTK with required modules...")        
+set(VTK_USE_FILE_INCLUDED 0)
+find_package(VTK COMPONENTS
+  vtkCommonCore
+  vtkRenderingCore
+  vtkRenderingOpenGL
+  vtkRenderingFreeType
+  vtkInteractionStyle
+  vtkIOImage
+  vtkImagingSources
+  vtkTestingRendering
+  vtkInteractionImage
+  vtkIOXML
+  REQUIRED)
+include(${VTK_USE_FILE})
+message(STATUS "ITKWikiExamples: VTK Found.")        
