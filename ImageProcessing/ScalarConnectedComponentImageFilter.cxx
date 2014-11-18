89c03b25ff209df6c9698aeaacd92717e01238d2
Bill Lorensen <bill.lorensen@gmail.com>
2014-11-18 11:41:52 -0500

    SYNC: Files modified on wiki.

--- ImageProcessing/ScalarConnectedComponentImageFilter.cxx
+++ ImageProcessing/ScalarConnectedComponentImageFilter.cxx
@@ -2,6 +2,7 @@
 #include "itkImageFileReader.h"
 #include "itkLabelToRGBImageFilter.h"
 #include "itkRelabelComponentImageFilter.h"
+#include "itkLabelStatisticsImageFilter.h"
 
 #include "itkScalarConnectedComponentImageFilter.h"
 
@@ -13,16 +14,20 @@
 template <typename TImage>
 static void CreateImage(TImage* const image);
 
+template<typename TImage, typename TLabelImage>
+static void SummarizeLabelStatistics (TImage* image,
+                                      TLabelImage* labelImage);
+
 int main( int argc, char *argv[])
 {
   const unsigned int Dimension = 2;
-  typedef unsigned char                          PixelType;
+  typedef short                                  PixelType;
   typedef itk::Image<PixelType, Dimension>       ImageType;
 
   typedef itk::RGBPixel<unsigned char>           RGBPixelType;
   typedef itk::Image<RGBPixelType, Dimension>    RGBImageType;
 
-  typedef unsigned short                         LabelPixelType;
+  typedef unsigned int                           LabelPixelType;
   typedef itk::Image<LabelPixelType, Dimension > LabelImageType;
 
   ImageType::Pointer image;
@@ -68,8 +73,11 @@ int main( int argc, char *argv[])
   relabel->SetMinimumObjectSize(minSize);
   relabel->Update();
 
+  SummarizeLabelStatistics (image.GetPointer(), relabel->GetOutput());
+
   typedef itk::LabelToRGBImageFilter<LabelImageType, RGBImageType> RGBFilterType;
-  RGBFilterType::Pointer rgbFilter = RGBFilterType::New();
+  RGBFilterType::Pointer rgbFilter =
+    RGBFilterType::New();
   rgbFilter->SetInput( relabel->GetOutput() );
 
   QuickView viewer;
@@ -134,3 +142,63 @@ void CreateImage(TImage* const image)
       }
     }
 }
+
+template<typename TImage, typename TLabelImage>
+void SummarizeLabelStatistics (TImage* image,
+                               TLabelImage* labelImage)
+{
+  typedef itk::LabelStatisticsImageFilter< TImage, TLabelImage >
+    LabelStatisticsImageFilterType;
+  typename LabelStatisticsImageFilterType::Pointer labelStatisticsImageFilter =
+    LabelStatisticsImageFilterType::New();
+  labelStatisticsImageFilter->SetLabelInput( labelImage );
+  labelStatisticsImageFilter->SetInput(image);
+  labelStatisticsImageFilter->UseHistogramsOn(); // needed to compute median
+  labelStatisticsImageFilter->Update();
+
+  std::cout << "Number of labels: "
+            << labelStatisticsImageFilter->GetNumberOfLabels() << std::endl;
+  std::cout << std::endl;
+  
+  typedef typename LabelStatisticsImageFilterType::ValidLabelValuesContainerType ValidLabelValuesType;
+  typedef typename LabelStatisticsImageFilterType::LabelPixelType LabelPixelType;
+
+  for(typename ValidLabelValuesType::const_iterator vIt = labelStatisticsImageFilter->GetValidLabelValues().begin();
+      vIt != labelStatisticsImageFilter->GetValidLabelValues().end();
+      ++vIt)
+    {
+    if ( labelStatisticsImageFilter->HasLabel(*vIt) )
+      {
+      LabelPixelType labelValue = *vIt;
+      std::cout << "Label: " << *vIt << std::endl;
+      std::cout << "\tmin: "
+                << labelStatisticsImageFilter->GetMinimum( labelValue )
+                << std::endl;
+      std::cout << "\tmax: "
+                << labelStatisticsImageFilter->GetMaximum( labelValue )
+                << std::endl;
+      std::cout << "\tmedian: "
+                << labelStatisticsImageFilter->GetMedian( labelValue )
+                << std::endl;
+      std::cout << "\tmean: "
+                << labelStatisticsImageFilter->GetMean( labelValue )
+                << std::endl;
+      std::cout << "\tsigma: "
+                << labelStatisticsImageFilter->GetSigma( labelValue )
+                << std::endl;
+      std::cout << "\tvariance: "
+                << labelStatisticsImageFilter->GetVariance( labelValue )
+                << std::endl;
+      std::cout << "\tsum: "
+                << labelStatisticsImageFilter->GetSum( labelValue )
+                << std::endl;
+      std::cout << "\tcount: "
+                << labelStatisticsImageFilter->GetCount( labelValue )
+                << std::endl;
+      std::cout << "\tregion: "
+                << labelStatisticsImageFilter->GetRegion( labelValue )
+                << std::endl;
+      }
+    }
+
+  }
