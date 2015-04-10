a1474eedaece0cbeb9888f1f020456c747dd2c14
Bill Lorensen <bill.lorensen@gmail.com>
2015-04-09 20:40:58 -0700

    SYNC: Files added to wiki.

---
+++ Iterators/ShapedNeighborhoodIterator.cxx
@@ -0,0 +1,78 @@
+#include "itkImage.h"
+#include "itkShapedNeighborhoodIterator.h"
+#include "itkImageRegionConstIterator.h"
+#include "itkNeighborhoodAlgorithm.h"
+#include "itkBinaryBallStructuringElement.h"
+#include "itkBinaryCrossStructuringElement.h"
+
+typedef itk::Image<int, 2>       ImageType;
+typedef ImageType::PixelType     PixelType;
+ 
+static void CreateImage(ImageType::Pointer image);
+ 
+int main(int, char*[])
+{
+  ImageType::Pointer image = ImageType::New();
+  CreateImage(image);
+   
+  typedef itk::BinaryBallStructuringElement<PixelType, 2>
+    StructuringElementType;
+  StructuringElementType::RadiusType elementRadius;
+  elementRadius.Fill(2);
+  
+  StructuringElementType structuringElement;
+  structuringElement.SetRadius(elementRadius);
+  structuringElement.CreateStructuringElement();
+
+  typedef itk::ShapedNeighborhoodIterator<ImageType> IteratorType;
+  IteratorType siterator(structuringElement.GetRadius(),
+                         image,
+                         image->GetLargestPossibleRegion());
+  
+  siterator.CreateActiveListFromNeighborhood(structuringElement);
+  siterator.NeedToUseBoundaryConditionOff();
+
+  IteratorType::IndexType location;
+  location[0] = 4;
+  location[1] = 5;
+  siterator.SetLocation(location);
+  IteratorType::Iterator i;
+  for (i = siterator.Begin(); !i.IsAtEnd(); ++i)
+    {
+    i.Set(1);
+    }
+  
+  // Now show the results
+  typedef itk::ImageRegionConstIterator<ImageType> ImageIteratorType;
+  ImageIteratorType imit(image, image->GetLargestPossibleRegion());
+  imit.GoToBegin();
+  unsigned int col = 0;
+  while( !imit.IsAtEnd() )
+    {
+    PixelType value = imit.Get();
+    ++imit;
+    ++col;
+    std::cout << value << " ";
+    if ((col % 10) == 0)
+      {
+      std::cout << std::endl;
+      }
+    }
+
+  return EXIT_SUCCESS;
+}
+ 
+void CreateImage(ImageType::Pointer image)
+{
+  ImageType::IndexType start;
+  start.Fill(0);
+ 
+  ImageType::SizeType size;
+  size.Fill(10);
+  
+  ImageType::RegionType region(start,size);
+
+  image->SetRegions(region);
+  image->Allocate();
+  image->FillBuffer(0);
+}
---
+++ Utilities/PassImageToFunction.cxx
@@ -0,0 +1,42 @@
+#include <itkImage.h>
+
+#include <iostream>
+
+typedef itk::Image<float, 2> ImageType;
+static void myStandardPointer(const ImageType* input)
+{
+}
+
+static void mySmartPointer(const ImageType::Pointer input)
+{
+}
+
+template <typename TImage>
+static void TemplateSmartPointer(const typename TImage::Pointer input)
+{
+}
+
+template <typename TImage>
+static void TemplateStandardPointer(const TImage* input)
+{
+}
+
+int main(int, char *[])
+{
+  ImageType::Pointer image = ImageType::New();
+  itk::Index<2> corner = {{0,0}};
+  itk::Size<2> size = {{10,10}};
+  itk::ImageRegion<2> region(corner,size);
+  image->SetRegions(region);
+  image->Allocate();
+
+  // A function that accepts a standard pointer can be passed either a standard or a smart pointer
+  myStandardPointer(image.GetPointer());
+  myStandardPointer(image);
+
+  // A function that accepts a smart pointer can be passed either a standard or a smart pointer
+  mySmartPointer(image.GetPointer());
+  mySmartPointer(image);
+
+  return 0;
+}
