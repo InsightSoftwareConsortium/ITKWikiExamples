d1d8210945b4301915fe1612e094cbefc82ff0ba
Bill Lorensen <lorensen@localhost.localdomain>
2015-05-14 14:00:19 -0400

    ENH: Added support for remote module

--- CMake/RequiresModule.cmake
+++ CMake/RequiresModule.cmake
@@ -6,3 +6,10 @@ macro(Requires_Module example module)
   endif()
 endmacro()
 
+macro(Requires_RemoteModule example module)
+  if(NOT ${module}_LOADED)
+    message(STATUS "ITKWikiExamples: ${example} requires the Remote module ${module} but ${module} is not loaded")
+    string(REGEX REPLACE "[^;]*${example}.cxx"
+           "" ALL_FILES "${ALL_FILES}")
+  endif()
+endmacro()
