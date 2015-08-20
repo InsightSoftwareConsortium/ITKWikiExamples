cbed42d7c3b8b8a264888db81c8c9cbbab671eea
Bill Lorensen <bill.lorensen@gmail.com>
2015-08-19 20:03:21 -0400

    SYNC: Files modified on wiki.

--- Meshes/PointAndCellData.cxx
+++ Meshes/PointAndCellData.cxx
@@ -1,49 +1,57 @@
+// Include the relevant header files.
 #include "itkMesh.h"
 #include "itkRegularSphereMeshSource.h"
 #include "itkMeshFileWriter.h"
 
+// We define the dimension and coordinate type...
 const unsigned int Dimension = 3;
 typedef float      TCoordinate;
 
+// ...and then typedef the mesh, sphere, and writer.
 typedef itk::Mesh< TCoordinate, Dimension >   TMesh;
 typedef itk::RegularSphereMeshSource< TMesh > TSphere;
 typedef itk::MeshFileWriter< TMesh >          TMeshWriter;
-
+ 
 int main()
 {
 
+  // Create the sphere source. 
   TSphere::Pointer sphere = TSphere::New();
   sphere->Update();
 
+  // We now assign it to a mesh pointer. 
   TMesh::Pointer mesh = sphere->GetOutput();
-  double value = 10;
-  for (unsigned int i = 0; i < mesh->GetNumberOfCells(); ++i)
-    mesh->SetCellData( i, value );
+  
+  // It is necessary to disconnect the mesh from the pipeline;
+  // otherwise, the point and cell data will be deallocated
+  // when we call "Update()" on the writer later in the program.
+  mesh->DisconnectPipeline();
 
+  // Let's assign a value to each of the mesh's points...
   for (unsigned int i = 0; i < mesh->GetNumberOfPoints(); ++i)
-    mesh->SetPointData( i, value );
+    mesh->SetPointData( i, 5.0 ); 
 
-  std::cout << mesh->GetNumberOfCells() << std::endl; // 128
-  std::cout << mesh->GetCellData() << std::endl; // A pointer
-  std::cout << mesh->GetCellData()->Size() << std::endl << std::endl; // 128
+  // ...and assign a different value to each of the mesh's cells.
+  for (unsigned int i = 0; i < mesh->GetNumberOfCells(); ++i)
+    mesh->SetCellData( i, 10.0 );
 
+  // We'll print out some data about the points...
   std::cout << mesh->GetNumberOfPoints() << std::endl; // 66
-  std::cout << mesh->GetPointData() << std::endl; // Another pointer
-  std::cout << mesh->GetPointData()->Size() << std::endl << std::endl; // 66
+  std::cout << mesh->GetPointData()->Size() << std::endl; // 66
+  std::cout << mesh->GetPointData()->ElementAt( 0 ) << std::endl << std::endl; // 5.0
+ 
+  // ...and about the cells.
+  std::cout << mesh->GetNumberOfCells() << std::endl; // 128
+  std::cout << mesh->GetCellData()->Size() << std::endl; // 128
+  std::cout << mesh->GetCellData()->ElementAt( 0 ) << std::endl << std::endl; // 10.0
 
+  // Finally, we'll write the data to file.  Note that the only mesh file
+  // formats supported by ITK which support cell and point data are .vtk and .gii.
   TMeshWriter::Pointer meshWriter = TMeshWriter::New();
   meshWriter->SetFileName( "mesh.vtk" );
   meshWriter->SetInput( mesh );
   meshWriter->Update();
-
-  std::cout << mesh->GetNumberOfCells() << std::endl; // 128
-  std::cout << mesh->GetCellData() << std::endl; // Different Pointer
-  std::cout << mesh->GetCellData()->Size() << std::endl << std::endl; // 0
-
-  std::cout << mesh->GetNumberOfPoints() << std::endl; // 66
-  std::cout << mesh->GetPointData() << std::endl; // Different pointer
-  std::cout << mesh->GetPointData()->Size() << std::endl << std::endl; // 0
-
+ 
   return EXIT_SUCCESS;
-
+ 
 }
