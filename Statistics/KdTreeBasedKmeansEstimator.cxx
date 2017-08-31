397230bb89cd6ea80ae5132e9b6a760d5b438fad
Bill Lorensen <bill.lorensen@gmail.com>
2017-08-30 20:03:39 -0400

    SYNC: Files modified on wiki.

--- Statistics/KdTreeBasedKmeansEstimator.cxx
+++ Statistics/KdTreeBasedKmeansEstimator.cxx
@@ -38,32 +38,30 @@ int main(int, char *[])
   mv[0] = 179.696; mv[1] = 176.696; mv[2] = 171.696;
   sample->PushBack(mv);
   
-  // If you uncomment these, it does not work (Exception: vectors are not the same size (3 and 0)
-//   mv[0] = 195.88; mv[1] = 191.88; mv[2] = 188.88;
-//   sample->PushBack(mv);
-//   mv[0] = 207.36; mv[1] = 206.36; mv[2] = 202.36;
-//   sample->PushBack(mv);
-//   mv[0] = 176.672; mv[1] = 176.008; mv[2] = 171.336;
-//   sample->PushBack(mv);
-//   mv[0] = 180.176; mv[1] = 181.176; mv[2] = 174.088;
-//   sample->PushBack(mv);
-//   mv[0] = 171.304; mv[1] = 172.304; mv[2] = 166.304;
-//   sample->PushBack(mv);
-//   mv[0] = 161; mv[1] = 162; mv[2] = 156.032;
-//   sample->PushBack(mv);
-//   mv[0] = 166.96; mv[1] = 167.96; mv[2] = 162.96;
-//   sample->PushBack(mv);
-//   mv[0] = 169; mv[1] = 170; mv[2] = 164.392;
-//   sample->PushBack(mv);
-//   mv[0] = 165.104; mv[1] = 166.104; mv[2] = 160.104;
-//   sample->PushBack(mv);
-//   mv[0] = 170.264; mv[1] = 171.264; mv[2] = 164.896;
-//   sample->PushBack(mv);
-//   mv[0] = 165.472; mv[1] = 165.944; mv[2] = 158.888;
-//   sample->PushBack(mv);
+  mv[0] = 195.88; mv[1] = 191.88; mv[2] = 188.88;
+  sample->PushBack(mv);
+  mv[0] = 207.36; mv[1] = 206.36; mv[2] = 202.36;
+  sample->PushBack(mv);
+  mv[0] = 176.672; mv[1] = 176.008; mv[2] = 171.336;
+  sample->PushBack(mv);
+  mv[0] = 180.176; mv[1] = 181.176; mv[2] = 174.088;
+  sample->PushBack(mv);
+  mv[0] = 171.304; mv[1] = 172.304; mv[2] = 166.304;
+  sample->PushBack(mv);
+  mv[0] = 161; mv[1] = 162; mv[2] = 156.032;
+  sample->PushBack(mv);
+  mv[0] = 166.96; mv[1] = 167.96; mv[2] = 162.96;
+  sample->PushBack(mv);
+  mv[0] = 169; mv[1] = 170; mv[2] = 164.392;
+  sample->PushBack(mv);
+  mv[0] = 165.104; mv[1] = 166.104; mv[2] = 160.104;
+  sample->PushBack(mv);
+  mv[0] = 170.264; mv[1] = 171.264; mv[2] = 164.896;
+  sample->PushBack(mv);
+  mv[0] = 165.472; mv[1] = 165.944; mv[2] = 158.888;
+  sample->PushBack(mv);
 
-  //typedef itk::Statistics::WeightedCentroidKdTreeGenerator< SampleType >
-  typedef itk::Statistics::KdTreeGenerator< SampleType > TreeGeneratorType;
+  typedef itk::Statistics::WeightedCentroidKdTreeGenerator< SampleType > TreeGeneratorType;
   TreeGeneratorType::Pointer treeGenerator = TreeGeneratorType::New();
 
   treeGenerator->SetSample( sample );
@@ -76,7 +74,10 @@ int main(int, char *[])
   EstimatorType::Pointer estimator = EstimatorType::New();
 
   EstimatorType::ParametersType initialMeans(2*3);
-  initialMeans.Fill(0.0f);
+  initialMeans.Fill(165.0f);
+  initialMeans[3] = 200;
+  initialMeans[4] = 200;
+  initialMeans[5] = 200;
 
   estimator->SetParameters( initialMeans );
   estimator->SetKdTree( treeGenerator->GetOutput() );
