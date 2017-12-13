ee09b9b7326f1ff867efbf086686a2b0258b6a87
Hans Johnson <hans-johnson@uiowa.edu>
2017-12-13 14:03:37 -0600

    STYLE: Convert CMake-language commands to lower case
    Ancient CMake versions required upper-case commands.  Later command
    names became case-insensitive.  Now the preferred style is lower-case.
    
    Run the following shell code:
    NOTE: MUST USE GNU compliant version of sed
    cmake --help-command-list \
    | grep -v "cmake version" \
    | while read c; do
        echo 's/\b'"$(echo $c | tr '[:lower:]' '[:upper:]')"'\(\s*\)(/'"$c"'\1(/g'
      done >convert.sed \
    && git ls-files -z -- bootstrap '*.cmake' '*.cmake.in' '*CMakeLists.txt' \
       | xargs -0 gsed -i -f convert.sed \
    && rm convert.sed

--- CMake/ExamplesTesting.cmake
+++ CMake/ExamplesTesting.cmake
@@ -1,17 +1,17 @@
 #
 # Create tests for examples
 #
-INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR})
-SET(MyTests "")
+include_directories(${CMAKE_CURRENT_SOURCE_DIR})
+set(MyTests "")
 foreach(SOURCE_FILE ${ALL_FILES})
-  STRING(REPLACE ".cxx" "" TMP ${SOURCE_FILE})
-  STRING(REPLACE ${CMAKE_CURRENT_SOURCE_DIR}/ "" EXAMPLE ${TMP})
-  SET(MyTests ${MyTests} Test${EXAMPLE}.cxx)
-  FILE(WRITE ${CMAKE_CURRENT_BINARY_DIR}/Test${EXAMPLE}.cxx
+  string(REPLACE ".cxx" "" TMP ${SOURCE_FILE})
+  string(REPLACE ${CMAKE_CURRENT_SOURCE_DIR}/ "" EXAMPLE ${TMP})
+  set(MyTests ${MyTests} Test${EXAMPLE}.cxx)
+  file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/Test${EXAMPLE}.cxx
     "#define main Test${EXAMPLE}\n#include \"${EXAMPLE}.cxx\"\n")
-  LIST(FIND NEEDS_ARGS ${EXAMPLE} SKIP_ADD)
+  list(FIND NEEDS_ARGS ${EXAMPLE} SKIP_ADD)
   if(SKIP_ADD EQUAL -1)
-    ADD_TEST(${KIT}-${EXAMPLE} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${KIT}CxxTests Test${EXAMPLE})
+    add_test(${KIT}-${EXAMPLE} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${KIT}CxxTests Test${EXAMPLE})
   endif(SKIP_ADD EQUAL -1)
   set_property(TEST ${KIT}-${EXAMPLE} PROPERTY LABELS WikiExamples)
 endforeach(SOURCE_FILE)
@@ -19,13 +19,13 @@ endforeach(SOURCE_FILE)
 if(${VTK_MAJOR_VERSION} GREATER 4 )
   ## Version is OK if greater than 5.5
 else()
-  MESSAGE(FATAL_ERROR "Need VTK version 5.5 or greater to include the necessary vtkTestingObjectFactory.cmake")
+  message(FATAL_ERROR "Need VTK version 5.5 or greater to include the necessary vtkTestingObjectFactory.cmake")
 endif()
 
 
-SET(VTK_BINARY_DIR ${ITKWikiExamples_BINARY_DIR})
-SET(VTK_DATA_ROOT ${ITKWikiExamples_SOURCE_DIR}/Testing)
-INCLUDE(${ITKWikiExamples_SOURCE_DIR}/CMake/vtkTestingObjectFactory.cmake)
-ADD_EXECUTABLE(${KIT}CxxTests ${KIT}CxxTests.cxx
+set(VTK_BINARY_DIR ${ITKWikiExamples_BINARY_DIR})
+set(VTK_DATA_ROOT ${ITKWikiExamples_SOURCE_DIR}/Testing)
+include(${ITKWikiExamples_SOURCE_DIR}/CMake/vtkTestingObjectFactory.cmake)
+add_executable(${KIT}CxxTests ${KIT}CxxTests.cxx
                ${MyTests})
-TARGET_LINK_LIBRARIES(${KIT}CxxTests ${KIT_LIBS})
+target_link_libraries(${KIT}CxxTests ${KIT_LIBS})
