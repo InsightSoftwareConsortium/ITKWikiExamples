7e1da0605e290f1cfb5278760f5adf72619d947d
Hans Johnson <hans-johnson@uiowa.edu>
2017-12-23 18:49:30 -0600

    COMP: Remove deprecated code.

--- Utilities/RandomPermutation.cxx
+++ Utilities/RandomPermutation.cxx
@@ -3,10 +3,6 @@
 int main(int, char*[])
 {
   itk::RandomPermutation rp(5);
-#if !defined(ITK_LEGACY_REMOVE)
-  std::cout << "Dump..." << std::endl;
-  rp.Dump();
-#endif
 
   std::cout << std::endl;
 
@@ -17,10 +13,6 @@ int main(int, char*[])
   std::cout << std::endl << std::endl;
   
   rp.Shuffle();
-#if !defined(ITK_LEGACY_REMOVE)
-  std::cout << "Dump..." << std::endl;
-  rp.Dump();
-#endif  
   std::cout << "After shuffle" << std::endl;
   for(unsigned int i = 0; i < 5; i++)
     {
