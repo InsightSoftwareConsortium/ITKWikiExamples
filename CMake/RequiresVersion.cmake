1933f9bd50ac8f616417b228f16bde96d1282917
Hans J. Johnson <hans-johnson@uiowa.edu>
2019-07-03 11:48:19 -0500

    STYLE: Remove deprecated content that is now maintained on ITKExamples.

--- CMake/RequiresVersion.cmake
+++
@@ -1,8 +0,0 @@
-macro(Requires_Version example version_min)
-set(ITK_VERSION "${ITK_VERSION_MAJOR}.${ITK_VERSION_MINOR}.${ITK_VERSION_PATCH}")
-  if(ITK_VERSION VERSION_LESS "${version_min}")
-  message(STATUS "ITKWikiExamples: ${example} requires ITK version ${version_min} or newer but the current version is ${ITK_VERSION}")
-  string(REGEX REPLACE "[^;]*${example}.cxx"
-         "" ALL_FILES "${ALL_FILES}")
-  endif()
-endmacro()
